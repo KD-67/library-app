59a407855f36014d39a0515e7f02fe321888857a
KD-67 <kevin.a.dominey@gmail.com>
2023-06-06 20:16:09 +0300

    functional remove buttons (chatGPT solution, don't fully understand how it works tbh

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ const bookInfo = document.querySelector(".book-info");
 
 const addBookForm = document.querySelector(".add-book-form");
 
+const rmBtn = document.querySelector(".rmbtn");
+
 // Functions:
 
 let myLibrary = [];
@@ -21,13 +23,15 @@ function Book(title, author, finishedStatus) {
 }
 
 // adds info from newly created book to library list
-function displayNewBook(newBook) {
+function displayNewBook(newBook, index) {
   let newBookInfoEntry = document.createElement("li");
   let newBookInfo = document.createElement("ul");
   let newTitleDisplay = document.createElement("li");
   let newAuthorDisplay = document.createElement("li");
   let newFinishedStatusDisplay = document.createElement("li");
   let newRmBtn = document.createElement("button");
+  const bookID = createBookID(index);
+  newBookInfoEntry.setAttribute("id", bookID);
 
   newTitleDisplay.textContent = newBook.title;
   newAuthorDisplay.textContent = newBook.author;
@@ -40,11 +44,13 @@ function displayNewBook(newBook) {
   newBookInfo.appendChild(newFinishedStatusDisplay);
   newBookInfoEntry.appendChild(newRmBtn);
   newRmBtn.innerHTML = "Remove";
+  newRmBtn.addEventListener("click", function () {
+    removeBook(bookID);
+  });
 
   newTitleDisplay.innerHTML = "Title: " + newBook.title;
   newAuthorDisplay.innerHTML = "Author: " + newBook.author;
-  newFinishedStatusDisplay.innerHTML =
-    "Finished Status: " + newBook.finishedStatus;
+  newFinishedStatusDisplay.innerHTML = "Finished? " + newBook.finishedStatus;
 }
 
 // adds newly created book object to myLibrary array
@@ -52,14 +58,31 @@ function addBookToLibrary(newBook) {
   myLibrary.push(newBook);
 }
 
+// idenfifies each book
+function createBookID(index) {
+  return "book-" + index;
+}
+
+// remove book element from DOM and myLibrary
+function removeBook(bookID) {
+  const bookIndex = parseInt(bookID.split("-")[1]);
+  myLibrary.splice(bookIndex, 1);
+  const bookElement = document.getElementById(bookID);
+  bookElement.remove();
+}
+
 // form submit action: construct new book object, add it to library, post it to library list
 addBookForm.addEventListener("submit", function (event) {
   event.preventDefault();
-  let book1 = new Book(
+  let newBook = new Book(
     titleInput.value,
     authorInput.value,
     finishedStatusInput.value
   );
-  addBookToLibrary(book1);
-  displayNewBook(book1);
+  addBookToLibrary(newBook);
+  displayNewBook(newBook, myLibrary.length - 1);
+
+  titleInput.value = null;
+  authorInput.value = null;
+  finishedStatusInput.value = null;
 });
